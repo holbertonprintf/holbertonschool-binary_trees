16a3dbd0d103e15710ec3ee5c1947b63b42d6a32
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-01-09 05:18:02 -0800

    correction bux in the binary tree delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,8 +9,9 @@ void binary_tree_delete(binary_tree_t *tree)
 {
     if (tree == NULL)
         return;
-
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
+    if (tree->left != NULL)
+        binary_tree_delete(tree->left);
+    if (tree->right != NULL)
+        binary_tree_delete(tree->right);
     free(tree);
 }
